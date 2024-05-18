f53fc55b1cc6afa78945e01208e6feffe669e9f3
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-05-18 18:46:18 -0300

    finished header

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+// const btn = document.querySelector('.header__btn-mobile');
+// const container = document.querySelector('.header__nav-container');
+
+// btn.addEventListener('change', () => {
+//     container.classList.toggle('height');
+// })
