9ade58f53ec3d564973df78c0fc2c7f7830d0ba6
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-05-26 13:02:54 -0300

    "menu" section adjustments

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,15 @@ const container = document.querySelector('.header__nav-container');
 btn.addEventListener('change', () => {
     container.classList.toggle('height');
 })
+
+
+const menuBtn = document.querySelectorAll('.menu__button-food');
+
+menuBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+       menuBtn.forEach(btn => btn.classList.remove('menu-btn-checked'))
+
+        btn.classList.add('menu-btn-checked')
+        
+    })
+})
