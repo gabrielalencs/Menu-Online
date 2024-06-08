c43dbf4747ba36d6c7299d9c4ad17e6d447e9c69
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-06-08 20:17:37 -0300

    separating food button logic from the menu section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,22 @@
 // * imports
 
-import { mobileMenuBtn, toggleMenuMobile } from './header/menuMobile.js'
+import { mobileMenuBtn, toggleMenuMobile } from './header/menuMobile.js';
+
+import { markBtnClicked } from './menu/foodButtons.js';
 
 
+
+// * functions
+
+// ? menu
+
+markBtnClicked() 
 
 
 
 // * events
 
 mobileMenuBtn.addEventListener('change', toggleMenuMobile);
-
-
-
 
 
 
--- assets/js/menu/foodButtons.js
+++ assets/js/menu/foodButtons.js
@@ -1,16 +1,17 @@
-const foodButtons = document.querySelectorAll('menu__button-food');
+const foodButtons = document.querySelectorAll('.menu__button-food');
 
-foodButtons.forEach(button => {
-   button.addEventListener('click', () => {
+export const markBtnClicked = () => {
 
-   });
-});
+   foodButtons.forEach(button => {
 
-menuBtn.forEach(btn => {
-    btn.addEventListener('click', () => {
-       menuBtn.forEach(btn => btn.classList.remove('menu-btn-checked'))
+      button.addEventListener('click', () => {
 
-        btn.classList.add('menu-btn-checked')
-        
-    })
-})
+         foodButtons.forEach(button => button.classList.remove('menu-btn-checked'))
+
+         button.classList.add('menu-btn-checked');
+
+      });
+
+   })
+
+};
