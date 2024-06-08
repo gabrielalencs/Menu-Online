2a81413dd2d604cea92440db59ddcbca81ae7fc5
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-06-08 19:42:49 -0300

    header js code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,23 @@
-const btn = document.querySelector('.header__btn-mobile');
-const container = document.querySelector('.header__nav-container');
+import { mobileMenuBtn, toggleMenuMobile } from './header/menuMobile.js'
 
-btn.addEventListener('change', () => {
-    container.classList.toggle('height');
-})
+mobileMenuBtn.addEventListener('change', toggleMenuMobile);
 
 
-const menuBtn = document.querySelectorAll('.menu__button-food');
 
-menuBtn.forEach(btn => {
-    btn.addEventListener('click', () => {
-       menuBtn.forEach(btn => btn.classList.remove('menu-btn-checked'))
 
-        btn.classList.add('menu-btn-checked')
+
+
+
+
+
+
+// const menuBtn = document.querySelectorAll('.menu__button-food');
+
+// menuBtn.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//        menuBtn.forEach(btn => btn.classList.remove('menu-btn-checked'))
+
+//         btn.classList.add('menu-btn-checked')
         
-    })
-})
+//     })
+// })
