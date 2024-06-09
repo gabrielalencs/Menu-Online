4f885513c44b6c50e97ad0c962fae604859206fd
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-06-08 21:45:53 -0300

    scripts to add or decrease cart items refactor

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,64 +1,20 @@
 // * imports
 
-// ? header
-
 import { mobileMenuBtn, toggleMenuMobile } from './header/menuMobile.js';
 
-// ? menu
-
 import markBtnClicked from './menu/foodButtons.js';
+
+import addOrRemoveItems from './menu/addItemsToCart.js'
 
 
 // * functions
 
-// ? menu
+markBtnClicked() // ? marks which food button the user clicked on
 
-markBtnClicked()
+addOrRemoveItems() // ? increment or decrement menu items before adding to cart
+
 
 
 // * events
 
-// ? header
-
 mobileMenuBtn.addEventListener('change', toggleMenuMobile);
-
-
-const buttonsDecreaseItems = document.querySelectorAll('.btn-menu-minus');
-
-const itemCountersElement = document.querySelectorAll('.menu__food-count');
-
-const buttonsAddItems = document.querySelectorAll('.btn-menu-plus');
-
-
-let itemCountersArray = Array(itemCountersElement.length).fill(0);
-
-
-buttonsDecreaseItems.forEach((buttonMinus, currentIndex) => {
-
-    buttonMinus.addEventListener('click', () => {
-
-        if (itemCountersArray[currentIndex] > 0) {
-
-            itemCountersArray[currentIndex]--;
-
-            itemCountersElement[currentIndex].textContent = itemCountersArray[currentIndex];
-
-        }
-
-    });
-
-});
-
-
-buttonsAddItems.forEach((buttonPlus, currentIndex) => {
-
-    buttonPlus.addEventListener('click', () => {
-
-        itemCountersArray[currentIndex]++;
-
-        itemCountersElement[currentIndex].textContent = itemCountersArray[currentIndex];
-
-    });
-
-});
-
