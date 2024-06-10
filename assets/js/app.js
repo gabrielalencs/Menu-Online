4d92aed5aecb0d66211f82ac48fea45cc143066e
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-06-10 20:39:10 -0300

    json image path settings

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,42 @@ addOrRemoveItems() // ? increment or decrement menu items before adding to cart
 // * events
 
 mobileMenuBtn.addEventListener('change', toggleMenuMobile);
+
+
+
+const foodButtons = document.querySelectorAll('.menu__button-food');
+
+const menuFood = document.querySelectorAll('.menu__food');
+
+const menuFoodTitle = document.querySelectorAll('.menu__food .menu__food-title');
+
+const menuFoodPrice = document.querySelectorAll('.menu__food .menu__food-price');
+
+const menuFoodImg = document.querySelectorAll('.menu__food .menu__food-img');
+
+
+let arr = []
+
+fetch('../../assets/data/foods.json').then(resolve => {
+
+    return resolve.json()
+
+}).then(body => {
+
+    let arrayDados = body;
+
+
+    let burgues = arrayDados.burgers;
+
+    // let pizzas = arrayDados.pizzas;
+    // let barbecue = arrayDados.barbecue;
+
+    burgues.forEach((element, index) => {
+        menuFoodTitle[index].textContent = element.name;
+
+        menuFoodPrice[index].textContent = element.price;
+
+        menuFoodImg[index].src = element.image;
+    });
+
+})
