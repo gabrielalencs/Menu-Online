95b98afafc8518e481763d9e57138efc036bfa74
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-06-14 16:01:05 -0300

    adds zip code search functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,58 @@ initializeFoodMenu(); // ? initializes the menu logic, calling all functions for
 
 
 
+const zipCodeInput = document.getElementById('cep');
+
+const addressInput = document.getElementById('address');
+
+const neighborhoodInput = document.getElementById('neighborhood');
+
+const ufInput = document.getElementById('uf');
+
+const cityInput = document.getElementById('city');
+
+const btnZipCodeSearch = document.getElementById('zip-code-search');
+
+
+const chamaApiCep = async (cep) => {
+
+    if (!/^\d{8}$/.test(cep)) {
+        alert("CEP inválido. Deve conter 8 dígitos numéricos.");
+        return null;
+    }
+
+    const responseApi = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+
+    const responseJson = await responseApi.json();
+
+    if (responseJson.erro) {
+
+        alert("CEP não encontrado. Digite as informações manualmente.");
+
+        return
+
+    }
+
+    return responseJson;
+}
+
+btnZipCodeSearch.addEventListener('click', async () => {
+
+    const valueInputCep = zipCodeInput.value.trim(); // Remove espaços em branco
+
+    let obj = await chamaApiCep(valueInputCep);
+
+    addressInput.value = obj.logradouro;
+
+    neighborhoodInput.value = obj.bairro;
+
+    cityInput.value = obj.localidade;
+
+    ufInput.value = obj.uf;
+
+});
+
+
 
 
 // * events
