30fb3f0de6bde9527a1ebb07e84e6a247e29c7c5
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-06-14 20:00:12 -0300

    completion of automatic filling of the form

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,9 @@ import { mobileMenuBtn, toggleMenuMobile } from './header/menuMobile.js';
 
 import markBtnClicked from './menu/foodButtons.js';
 
-import initializeFoodMenu from './menu/initializeFoodMenu.js'
+import initializeFoodMenu from './menu/initializeFoodMenu.js';
+
+import {btnZipCodeSearch, fillInputFields} from './shoppingCart/fillAddress.js'
 
 
 
@@ -17,57 +19,7 @@ markBtnClicked() // ? marks which food button the user clicked on
 initializeFoodMenu(); // ? initializes the menu logic, calling all functions for correct operation
 
 
-
-const zipCodeInput = document.getElementById('cep');
-
-const addressInput = document.getElementById('address');
-
-const neighborhoodInput = document.getElementById('neighborhood');
-
-const ufInput = document.getElementById('uf');
-
-const cityInput = document.getElementById('city');
-
-const btnZipCodeSearch = document.getElementById('zip-code-search');
-
-
-const chamaApiCep = async (cep) => {
-
-    if (!/^\d{8}$/.test(cep)) {
-        alert("CEP inválido. Deve conter 8 dígitos numéricos.");
-        return null;
-    }
-
-    const responseApi = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-
-    const responseJson = await responseApi.json();
-
-    if (responseJson.erro) {
-
-        alert("CEP não encontrado. Digite as informações manualmente.");
-
-        return
-
-    }
-
-    return responseJson;
-}
-
-btnZipCodeSearch.addEventListener('click', async () => {
-
-    const valueInputCep = zipCodeInput.value.trim(); // Remove espaços em branco
-
-    let obj = await chamaApiCep(valueInputCep);
-
-    addressInput.value = obj.logradouro;
-
-    neighborhoodInput.value = obj.bairro;
-
-    cityInput.value = obj.localidade;
-
-    ufInput.value = obj.uf;
-
-});
+btnZipCodeSearch.addEventListener('click', fillInputFields);
 
 
 
