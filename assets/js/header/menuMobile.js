eb07b43b47371c47a96dfc7f699bf14c8412361f
Gabriel de Alencar <gabrieldealencar.dev@gmail.com>
2024-06-20 08:54:02 -0300

    adding navigation links

--- assets/js/header/menuMobile.js
+++ assets/js/header/menuMobile.js
@@ -1,4 +1,12 @@
 export const mobileMenuBtn = document.getElementById('mobile-menu-btn');
 const menuContainer = document.getElementById('menu-container');
+const menuLinks = document.querySelectorAll('.header__list-item');
+const inputChekboxMenu = document.getElementById('checkbox');
 
 export const toggleMenuMobile = () => menuContainer.classList.toggle('height');
+
+
+menuLinks.forEach(link => link.addEventListener('click', () => {
+    inputChekboxMenu.checked = false
+    toggleMenuMobile();
+}))
