43d9a584c29619ac7e67402ec32aafe3e2c9a8c2
gabrielalencs <gabrieldealencar.dev@gmail.com>
2024-06-27 18:50:10 -0300

    add animation when clicking food button

--- assets/js/menu/clickFoodButtons.js
+++ assets/js/menu/clickFoodButtons.js
@@ -1,13 +1,16 @@
 export const foodButtons = document.querySelectorAll('.menu__button-food');
+const containerFoods = document.querySelector('.menu__foods-container');
 
 const markBtnClicked = () => {
     foodButtons.forEach(button => {
         button.addEventListener('click', () => {
             foodButtons.forEach(button => button.classList.remove('menu-btn-checked'));
-            
             button.classList.add('menu-btn-checked');
-        });
 
+            containerFoods.classList.remove('fade-in-up');
+            void containerFoods.offsetWidth; // Trigger reflow to restart the animation
+            containerFoods.classList.add('fade-in-up');
+        });
     })
 };
 
